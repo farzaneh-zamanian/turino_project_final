a7840948900eb84b896f5b3c99079cc96a6e6b4b
Farzaneh Zamanian <152534852+farzaneh-zamanian@users.noreply.github.com>
2024-12-27 22:33:14 +0330

    Modify atomic design to change some compoennets

--- front/components/ui/atoms/Icon.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-const Icon = ({ src, width, height }) => {
-  return (
-    <Image src={src} width={width} height={height} />
-  );
-};
-
-export default Icon;
--- front/components/ui/atoms/IconDescription.js
+++
@@ -1,7 +0,0 @@
-const IconDescription = ({ children, className }) => {
-      return (
-        <span className={className}>{children}</span>
-      );
-    };
-    
-    export default IconDescription;
