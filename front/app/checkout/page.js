2ae827912533ed55b1a1c7c6a83301c0db604f2e
Farzaneh Zamanian <152534852+farzaneh-zamanian@users.noreply.github.com>
2024-12-27 18:22:56 +0330

    Modify checkout page

--- front/app/checkout/page.js
+++ front/app/checkout/page.js
@@ -3,21 +3,23 @@ import { DatePicker } from "zaman";
 import React from 'react'
 import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 import Button from '@/components/ui/atoms/Button';
 import { personalInformationSchema } from '@/core/schema';
 import { useCheckout } from '@/core/services/mutations';
 import { useGetBasket } from '@/core/services/queries';
-import { DateToIso, flattenObject } from "@/core/utils/hepler";
-import Link from "next/link";
-import toast from "react-hot-toast";
-import { useRouter } from "next/navigation";
+import { calculateDuration, DateToIso, flattenObject } from "@/core/utils/hepler";
 
 
 function CheckOutPage() {
       const router = useRouter();
 
       const { data, isPending } = useGetBasket();//get tour information
+
+
       const { mutate } = useCheckout();//mutate passenger and tour information
       // react hook form
       const {
@@ -47,7 +49,7 @@ function CheckOutPage() {
 
       if (isPending) return <p>...Loading</p>
 
- 
+
       if (!data) return (
             <div className="flex flex-col gap-4 items-center justify-center my-[5rem]  w-6/12 h-[20rem] bg-accentButtonColor ">
                   <p className=' text-[2rem]'>سبد خرید شما خالی است</p>
@@ -58,82 +60,98 @@ function CheckOutPage() {
             </div>
       );
 
+
+
+
       return (
-            <div className='grid grid-cols-4 w-full gap-5 my-[5rem]'>
-                  <div className=' border-borderDivColor col-span-3 rounded-xl border'>
-                        <form onSubmit={handleSubmit(submitHandler)} className='flex flex-col   rounded-2xl p-[1.5rem] gap-[1rem] md:h-[28rem]'>
-                              <div>
-                                    <h2>مشخصات مسافر </h2>
-                                    <div className="flex flex-row flex-wrap item-center justify-between gap-10">
 
-                                          {/* name and national code */}
-                                          <div className="flex flex-col gap-2 flex-2">
+            <>
+                  <div className="container mx-auto py-12">
+                        <div  >
+                              <form onSubmit={handleSubmit(submitHandler)} className="grid  grid-cols-1 gap-8 md:grid-cols-4" >
+                                    <div className=" border border-borderDivColor rounded-xl md:col-span-3 p-[1rem] ">
 
-                                                <input
-                                                      {...register("fullName")}
-                                                      placeholder="نام و نام خانوادگی "
-                                                      maxLength={30}
-                                                      className="md:border md:rounded-[0.5rem] md:p-[0.8rem] w-[25rem] h-[4.5rem]"
+                                          <h2 className="text-[1.6rem] leading-[2.48rem] text-primary p-[1rem]">مشخصات مسافر  </h2>
+                                          <div className="md:flex justify-between gap-5">
+                                                <div className="flex flex-col gap-2">
+
+                                                      <input
+                                                            {...register("fullName")}
+                                                            placeholder="نام و نام خانوادگی "
+                                                            maxLength={30}
+                                                            className="border rounded-[0.5rem] p-[0.8rem] w-full md:w-[20rem] h-[4.5rem] text-left placeholder:text-right"
+                                                      />
+                                                      <span className="text-red-600 h-[1rem] text-[1rem] p-0">
+                                                            {!!errors?.fullName && errors?.fullName?.message}
+                                                      </span>
+                                                </div>
+                                                <div className="flex flex-col gap-2 ">
+                                                      <input
+                                                            {...register("nationalCode")}
+                                                            placeholder="کدملی"
+                                                            maxLength={10}
+                                                            className="border rounded-[0.5rem] p-[0.8rem] w-full md:w-[20rem] h-[4.5rem] text-left placeholder:text-right"
+                                                      />
+                                                      <span className="text-red-600 h-[1rem] text-[1rem] p-0">
+                                                            {!!errors?.nationalCode && errors?.nationalCode?.message}
+                                                      </span>
+                                                </div>
+
+                                                {/* select birth date */}
+                                                <Controller
+                                                      control={control}
+                                                      name="birthDate"
+                                                      render={({ field: { onChange } }) => (
+
+                                                            <DatePicker
+                                                                  accentColor="#28A745"
+                                                                  round="x2"
+                                                                  inputClass="w-full md:w-[25rem] h-[4.5rem] border md:rounded-[0.5rem]"
+                                                                  onChange={(e) =>
+                                                                        onChange({
+                                                                              birthDate: (DateToIso(e.value)).split('T')[0],
+                                                                        })
+
+                                                                  } />
+
+                                                      )}
                                                 />
-                                                <span className="text-red-600 h-[2rem] p-0">
-                                                      {!!errors?.fullName && errors?.fullName?.message}
+                                                <select defaultValue="" {...register("gender")} className="md:border md:rounded-[0.5rem] md:p-[0.8rem] w-[25rem] h-[4.5rem]">
+                                                      <option value="" disabled >جنسیت</option>
+                                                      <option value="male">مرد</option>
+                                                      <option value="female">زن</option>
+                                                </select>
+                                          </div>
+
+
+                                    </div>
+                                    <div className="border border-borderDivColor rounded-xl md:col-span-1 p-[1rem] ">
+                                          <div className="flex items-center justify-between">
+                                                <span className="text-[2.4rem]"> 
+                                                      {data?.data?.title}
+                                                </span>
+                                                <span className="text-[1.4rem] text-gray-500">{calculateDuration(data?.data?.startDate, data?.data?.endDate)}</span>
+
+                                          </div>
+                                          <div className="flex items-center justify-between">
+                                                <span className="text-[1.4rem] text-valueColor">قیمت نهایی</span>
+                                                <span className="text-[1.4rem]">
+                                                      {data?.data?.price}تومان
                                                 </span>
                                           </div>
-                                          <div className="flex flex-col gap-2 flex-2">
-                                                <input
-                                                      {...register("nationalCode")}
-                                                      placeholder="کدملی"
-                                                      maxLength={10}
-                                                      className="md:border md:rounded-[0.5rem] md:p-[0.8rem] w-[25rem] h-[4.5rem]"
-                                                />
-                                                <span className="text-red-600 h-[2rem] p-0">
-                                                      {!!errors?.nationalCode && errors?.nationalCode?.message}
-                                                </span>
+                                          <div className="flex items-center justify-center border-t pt-[1rem]">
+                                                <Button label="ثبت نهایی" status="profileBtn" type="reservation" />
+
                                           </div>
-
-                                          {/* select birth date */}
-                                          <Controller
-                                                control={control}
-                                                name="birthDate"
-                                                render={({ field: { onChange } }) => (
-                                                      <DatePicker
-                                                            accentColor="#28A745"
-                                                            round="x2"
-                                                            inputClass="w-[25rem] h-[4.5rem] border md:rounded-[0.5rem]"
-                                                            onChange={(e) =>
-                                                                  onChange({
-                                                                        birthDate: (DateToIso(e.value)).split('T')[0],
-                                                                  })
-                                                            } />
-                                                )}
-                                          />
-
-
-
-                                          {/* select gender */}
-                                          <select defaultValue="" {...register("gender")} className="md:border md:rounded-[0.5rem] md:p-[0.8rem] w-[25rem] h-[4.5rem]">
-                                                <option value="" disabled >جنسیت</option>
-                                                <option value="male">مرد</option>
-                                                <option value="female">زن</option>
-                                          </select>
 
                                     </div>
 
-
-                              </div>
-                              <div className='border-borderDivColor col-span-1 rounded-xl border' >
-                                    {data?.data?.title}
-                                    {/* button actions */}
-                                    <div className="flex items-center justify-end gap-2 md:border-t md:pt-[2rem]">
-                                          <Button label="ثبت و خرید نهایی" status="profileBtn" type="submit" />
-
-                                    </div>
-                              </div>
-
-                        </form>
+                              </form>
+                        </div>
                   </div>
 
-            </div>
+            </>
+
 
       )
 }
